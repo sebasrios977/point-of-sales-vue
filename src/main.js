c9fa96c6c9216531a537eb4d2c90da4464f4995e
sebasrios97 <sebastianriosrico@gmail.com>
2025-08-08 14:03:09 -0500

    21th section completed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { plugin, defaultConfig } from '@formkit/vue';
+import config from '../formkit.config';
 
 import App from './App.vue';
 import '@/assets/main.css';
 import router from './router';
 
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(plugin, defaultConfig(config));
 app.use(router)
+
+// Firebase
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+})
 
 app.mount('#app')
--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,21 +1,69 @@
-
 <script setup>
-import Link from '@/components/Link.vue';
-
+import Link from "@/components/Link.vue";
 </script>
 
 <template>
-    <div>
-        <Link
-            to="products"
-        >
-            Volver
-        </Link>
+  <div>
+    <Link to="products"> Volver </Link>
 
-        <h1 class="text-4xl font-black my-10">Nuevo producto</h1>
+    <h1 class="text-4xl font-black my-10">Nuevo producto</h1>
+
+    <div class="flex justify-center bg-white shadow">
+      <div class="mt-10 p-10 w-full 2xl:w-2/4">
+        <FormKit 
+            type="form" 
+            submit-label="Agregar Producto"
+        >
+          <FormKit 
+            type="text" 
+            label="Nombre" 
+            name="name" 
+            placeholder="Nombre de Producto" 
+            validation="required" 
+            :validation-messages="{required: 'El nombre del producto es obligatorio'}"
+          />
+
+          <FormKit 
+            type="file" 
+            label="Imagen Producto" 
+            name="image" 
+            validation="required" 
+            :validation-messages="{required: 'La imagen del producto es obligatoria'}"
+            accept=".jpg"
+          />
+
+          <FormKit 
+            type="select" 
+            label="Categoría" 
+            name="category" 
+            validation="required" 
+            :validation-messages="{required: 'La categoría es obligatoria'}"
+            :options="[1, 2, 3]"
+          />
+
+          <FormKit 
+            type="number" 
+            label="Precio"
+            name="price" 
+            placeholder="Precio de Producto"
+            validation="required" 
+            :validation-messages="{required: 'El precio es obligatorio'}"
+            min="1"
+          />
+
+          <FormKit 
+            type="number" 
+            label="Disponibles"
+            name="availability" 
+            placeholder="Cantidad disponible"
+            validation="required" 
+            :validation-messages="{required: 'La cantidad es obligatoria'}"
+            min="1"
+          />
+        </FormKit>
+      </div>
     </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
